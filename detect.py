5b991468a7e6ad335dee828c1991bf84b7bb6fc3
Sibylle <sibylle.azeau@grenoble-inp.org>
2025-03-05 16:47:51 +0100

    nettoyage

--- detect.py
+++ detect.py
@@ -68,9 +68,9 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 @smart_inference_mode()
 def run(
-    weights=ROOT / "/content/last.pt",  # model path or triton URL
-    source=ROOT / "/content/drive/MyDrive/coin-detection/test/images",  # file/dir/URL/glob/screen/0(webcam)
-    data=ROOT / "/content/drive/MyDrive/coin-detection/data.yaml",  # dataset.yaml path
+    weights="/content/last.pt",  # model path or triton URL
+    source="/content/drive/MyDrive/DATA_DIR/r1_test_b/Images",  # file/dir/URL/glob/screen/0(webcam)
+    #data="/content/drive/MyDrive/coin-detection/data.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
     conf_thres=0.25,  # confidence threshold
     iou_thres=0.45,  # NMS IOU threshold
